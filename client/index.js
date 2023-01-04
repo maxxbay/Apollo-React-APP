9602d9ae2b1e3cde635f83a9684fb7b89ce2ce68
Maxxbay <maxxbay@gmail.com>
2023-01-04 18:18:21 +0100

    env

--- client/index.js
+++ client/index.js
@@ -1,17 +1,20 @@
-import './style/style.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory, IndexRoute } from 'react-router';
-import ApolloClient from 'apollo-client';
-import { ApolloProvider } from 'react-apollo';
+import "./style/style.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router, Route, hashHistory, IndexRoute } from "react-router";
+import ApolloClient from "apollo-client";
+import { ApolloProvider } from "react-apollo";
 
-import App from './components/App';
-import SongList from './components/SongList';
-import SongCreate from './components/SongCreate';
-import SongDetail from './components/SongDetail';
+import App from "./components/App";
+import SongList from "./components/SongList";
+import SongCreate from "./components/SongCreate";
+import SongDetail from "./components/SongDetail";
 
 const client = new ApolloClient({
-  dataIdFromObject: o => o.id
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+
+  dataIdFromObject: (o) => o.id,
 });
 
 const Root = () => {
@@ -28,7 +31,4 @@ const Root = () => {
   );
 };
 
-ReactDOM.render(
-  <Root />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<Root />, document.querySelector("#root"));
